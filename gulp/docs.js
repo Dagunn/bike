418ab4ed1944f80d86bbc61e760396c323a3aa1a
Dagunn <54283683+Dagunn@users.noreply.github.com>
2024-01-19 23:25:49 +0400

    finish
    finish

--- gulp/docs.js
+++ gulp/docs.js
@@ -3,7 +3,7 @@ const gulp = require('gulp');
 
 // HTML
 const fileInclude = require('gulp-file-include');
-const htmlClean = require ('gulp-htmlclean');
+const htmlClean = require('gulp-htmlclean');
 const webpHTML = require('gulp-webp-html');
 //HTML
 
@@ -25,22 +25,22 @@ const notify = require('gulp-notify');
 const webpack = require('webpack-stream');
 const babel = require('gulp-babel');
 const webp = require('gulp-webp');
-const imageMin = require('gulp-imagemin');
+// const imageMin = require('gulp-imagemin');
 const changed = require('gulp-changed');
 
 // const media = require('gulp-group-css-media-queries');
 
 
 
-gulp.task('clean:docs', function(done){
-  if(fs.existsSync('./docs/')) {
-    return gulp.src('./docs/', {read: false})
-    .pipe(clean({force: true}));
+gulp.task('clean:docs', function (done) {
+  if (fs.existsSync('./docs/')) {
+    return gulp.src('./docs/', { read: false })
+      .pipe(clean({ force: true }));
   }
   done();
 })
 
-const notifyPlumber = (title) =>{
+const notifyPlumber = (title) => {
   return {
     errorHandler: notify.onError({
       title: title,
@@ -49,7 +49,7 @@ const notifyPlumber = (title) =>{
     }),
   };
 }
-gulp.task('html:docs', function(){
+gulp.task('html:docs', function () {
   return gulp.src('./src/*.html')
     .pipe(changed('./docs/'))
     .pipe(plumber(notifyPlumber('HTML')))
@@ -62,13 +62,13 @@ gulp.task('html:docs', function(){
     .pipe(gulp.dest('./docs/'))
 })
 
-gulp.task('sass:docs', function(){
+gulp.task('sass:docs', function () {
   return gulp
     .src('./src/scss/*.scss')
     .pipe(changed('./docs/css/'))
     .pipe(notify(notifyPlumber('scss')))
     .pipe(source.init())
-    .pipe(autoprefixer({overrideBrowserslist: ['last 10 version']}))
+    .pipe(autoprefixer({ overrideBrowserslist: ['last 10 version'] }))
     .pipe(sassGlob())
     .pipe(webpCss())
     .pipe(sass())
@@ -78,31 +78,31 @@ gulp.task('sass:docs', function(){
     .pipe(gulp.dest('./docs/css/'))
 })
 
-gulp.task('imgs:docs', function(){
+gulp.task('imgs:docs', function () {
   return gulp.src('./src/img/**/*')
     .pipe(changed('./docs/img/'))
     .pipe(webp())
     .pipe(gulp.dest('./docs/img/'))
     .pipe(gulp.src('./src/img/**/*'))
     .pipe(changed('./docs/img/'))
-    .pipe(imageMin({verbose: true}))
+    // .pipe(imageMin({verbose: true}))
     .pipe(gulp.dest('./docs/img/'))
 
 })
 
-gulp.task('fonts:docs', function(){
+gulp.task('fonts:docs', function () {
   return gulp.src('./src/fonts/**/*')
     .pipe(changed('./docs/fonts/'))
     .pipe(gulp.dest('./docs/fonts/'))
 })
 
-gulp.task('files:docs', function(){
+gulp.task('files:docs', function () {
   return gulp.src('./src/files/**/*')
     .pipe(changed('./docs/files/'))
     .pipe(gulp.dest('./docs/files/'))
 })
 
-gulp.task('js:docs', function(){
+gulp.task('js:docs', function () {
   return gulp
     .src('./src/js/*.js')
     .pipe(changed('./docs/js/'))
@@ -113,7 +113,7 @@ gulp.task('js:docs', function(){
 
 })
 
-gulp.task('server:docs', function(){
+gulp.task('server:docs', function () {
   return gulp.src('./docs')
     .pipe(liveServer({
       livereload: true,
